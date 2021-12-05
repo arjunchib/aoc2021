0e3562fe0dfcfa6041fba245acdcb00ae308aae1
Arjun Chib <arjunpatrickchib@gmail.com>
2021-12-04 23:06:41 -0500

    Cleanup day 4

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("part 2: {}", calc2(include_str!("real.in")));
 }
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq)]
 struct Board {
     spaces: HashMap<usize, (usize, usize)>,
     rows: [usize; 5],
@@ -19,114 +19,94 @@ struct Board {
 }
 
 impl Board {
-    // returns true if the mark created a bingo!
-    fn mark(&mut self, num: usize) -> (bool, usize) {
+    fn from(input: String) -> Board {
+        let spaces = HashMap::from_iter(
+            input
+                .split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .enumerate()
+                .map(|(i, x)| (x, (i / 5, i % 5))),
+        );
+        Board {
+            spaces,
+            rows: [0; 5],
+            cols: [0; 5],
+            bingo: false,
+        }
+    }
+
+    fn mark(&mut self, num: usize) {
         if !self.spaces.contains_key(&num) || self.bingo {
-            return (false, 0);
+            return;
         }
         let (c, r) = self.spaces.remove(&num).unwrap();
         self.rows[r] += 1;
         self.cols[c] += 1;
         self.bingo = self.rows[r] == 5 || self.cols[c] == 5;
-        let score = self.spaces.keys().sum();
-        (self.bingo, score)
+    }
+
+    fn score(&self) -> usize {
+        self.spaces.keys().sum()
+    }
+}
+
+struct Game {
+    boards: Vec<Board>,
+    nums: Vec<usize>,
+}
+
+impl Game {
+    fn from(input: &str) -> Game {
+        let mut lines = input.lines();
+        let nums = lines
+            .next()
+            .unwrap()
+            .split(',')
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        let boards = lines
+            .skip(1)
+            .collect::<Vec<&str>>()
+            .split(|x| *x == "")
+            .map(|x| x.join(" "))
+            .map(|x| Board::from(x))
+            .collect();
+        Game { nums, boards }
+    }
+
+    fn find_best(&mut self) -> Option<usize> {
+        for n in self.nums.iter() {
+            self.boards.iter_mut().for_each(|b| b.mark(*n));
+            match self.boards.iter().find(|b| b.bingo) {
+                Some(b) => return Some(b.score() * n),
+                None => {}
+            }
+        }
+        None
+    }
+
+    fn find_worst(&mut self) -> Option<usize> {
+        let mut worst: Option<usize> = None;
+        for n in self.nums.iter() {
+            self.boards.retain(|b| !b.bingo);
+            self.boards.iter_mut().for_each(|b| b.mark(*n));
+            match self.boards.iter().find(|b| b.bingo) {
+                Some(b) => {
+                    worst = Some(b.score() * n);
+                }
+                None => {}
+            }
+        }
+        worst
     }
 }
 
 fn calc1(input: &str) -> usize {
-    let mut lines = input.lines();
-    let mut nums = lines
-        .next()
-        .unwrap()
-        .split(',')
-        .map(|x| x.parse::<usize>().unwrap());
-    let mut boards = vec![];
-    let mut spaces = HashMap::new();
-    let mut r = 0;
-    while let Some(line) = lines.next() {
-        if line == "" {
-            continue;
-        }
-        line.split_whitespace()
-            .map(|x| x.parse::<usize>().unwrap())
-            .enumerate()
-            .for_each(|(c, x)| {
-                spaces.insert(x, (c, r));
-            });
-        r += 1;
-        if r == 5 {
-            boards.push(Board {
-                spaces,
-                rows: [0; 5],
-                cols: [0; 5],
-                bingo: false,
-            });
-            spaces = HashMap::new();
-            r = 0;
-        }
-    }
-    while let Some(num) = nums.next() {
-        let result = boards
-            .iter_mut()
-            .map(|board| board.mark(num))
-            .find(|(bingo, _)| *bingo == true);
-        match result {
-            Some((_, score)) => {
-                return num * score;
-            }
-            None => {}
-        }
-    }
-    0
+    let mut game = Game::from(input);
+    game.find_best().unwrap()
 }
 
 fn calc2(input: &str) -> usize {
-    let mut lines = input.lines();
-    let mut nums = lines
-        .next()
-        .unwrap()
-        .split(',')
-        .map(|x| x.parse::<usize>().unwrap());
-    let mut boards = vec![];
-    let mut spaces = HashMap::new();
-    let mut r = 0;
-    while let Some(line) = lines.next() {
-        if line == "" {
-            continue;
-        }
-        line.split_whitespace()
-            .map(|x| x.parse::<usize>().unwrap())
-            .enumerate()
-            .for_each(|(c, x)| {
-                spaces.insert(x, (c, r));
-            });
-        r += 1;
-        if r == 5 {
-            boards.push(Board {
-                spaces,
-                rows: [0; 5],
-                cols: [0; 5],
-                bingo: false,
-            });
-            spaces = HashMap::new();
-            r = 0;
-        }
-    }
-    let mut worst = 0;
-    while let Some(num) = nums.next() {
-        let score: isize =
-            boards
-                .iter_mut()
-                .map(|board| board.mark(num))
-                .fold(-1, |mut acc, (bingo, score)| {
-                    if bingo {
-                        acc = score as isize;
-                    }
-                    acc
-                });
-        if score >= 0 {
-            worst = score as usize * num;
-        }
-    }
-    worst
+    let mut game = Game::from(input);
+    game.find_worst().unwrap()
 }
